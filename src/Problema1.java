ab84e27379896da1780ca580431aba2632e6f841
Oderay Olivert Santoys <oderay.olivertsantoys@iesfm.onmicrosoft.com>
2022-03-04 11:31:41 +0100

    Preparación de Entorno de Trabajo

---
+++ src/Problema1.java
@@ -0,0 +1,15 @@
+public class Problema1 {
+
+    private boolean esMultiploDeTres (Integer n){
+        return n % 3 == 0;
+    }
+    private boolean esMultiploDeCinco (Integer n){
+        return n % 5 == 0;
+    }
+
+    static public String cadenaTexto() {
+        String cadenaText[] = new String[20];
+
+        if ( )
+    }
+}
---
+++ test/TestsProblema1.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class TestsProblema1 {
+    @Test
+    public void cadenaTextos () {
+        Problema1 problema = new Problema1();
+        String cadenaText[] = new String[20];
+        int num = 0;
+
+        for (int i = 0; i > 20; i++) {
+            cadenaText[i] = Problema1.cadenaTexto();
+
+        }
+
+    }
+
+
+
+
+}
